67ebd120de3a9179b6f5a710f9c0abd585083dbe
Danilo D'Antoni <thomasd@danilo.homenet.telecomitalia.it>
2025-07-05 19:55:02 +0200

    Fix formula path and sha256

--- Formula/sphp.rb
+++ Formula/sphp.rb
@@ -1,7 +1,7 @@
 class Sphp < Formula
   desc "Simple PHP version switcher for macOS (Apache + CLI) using Homebrew"
-  homepage "https://github.com/ThomasDanilo96/brew-sphp"
-  url "https://raw.githubusercontent.com/ThomasDanilo96/homebrew-sphp/main/sphp.sh"
+  homepage "https://github.com/ThomasDanilo96/homebrew-sphp"
+  url "https://raw.githubusercontent.com/ThomasDanilo96/brew-sphp/master/sphp.sh"
   version "1.0.0"
   sha256 "54b1e4368d05619c1a310e2277e442c0143d5a0186ab814307439af7800930d3"
   license "MIT"
